7b81d0acf22a64c1a106b9f4a76715e83f646e30
B4BF00LN4MU <boy566@naver.com>
2024-06-11 09:39:23 +0900

    ddd

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	int b = 3;
+	int add = a + b;
+	int sub = a - b;
+	int mul = a * b;
+	int div = a / b;
+	int mod = a % b;
+
+	cout << a + b = add<< endl;
+	return 0;
+}
